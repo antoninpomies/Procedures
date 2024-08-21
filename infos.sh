c317fabd98ebb581e68c22862bb3cfa6d15d3d22
Antonin POMIES <95703047+antoninpomies@users.noreply.github.com>
2024-08-21 22:49:43 +0200

    Update infos.sh

--- infos.sh
+++ infos.sh
@@ -4,37 +4,91 @@ hostname=$(cat /etc/hostname)
 distrib=$(grep '^PRETTY_NAME' /etc/os-release | cut -d'"' -f2)
 user=$USER
 
+sudo truncate -s 0 /etc/motd
+
 asciiDebian="
-                                            %%%%%%%%%%%%                                            
-                                        #%%%%%%%%%%%%%%%%%%%%%%                                     
-                                     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                  
-                                   %%%%%%%%%%%%%        %%%%%%%%%%%%                                
-                                 %%%%%%%%%%%%               %%%%%%%%%%                              
-                                %%%%%%%%%                     %%%%%%%%%                             
-                             % %%%%%%%                          %%%%%%%%                            
-                             %%%%%%%                             %%%%%%%                            
-                             %%%%%                %%%%%%%         %%%%%%                            
-                           %%%%%%              %%%%      %%       %%%%% %                           
-                           %%%%%              %%%                  %%%%                             
-                           %%%%%             %%                    %%%%%                            
-                           %%%%%            %%%             %%     %%%%                             
-                           %%%%             %%%                    %%%%                             
-                           %%%%             %%%            %      %%%%                              
-                           %%%%             %%%%                 %%%%                               
-                           %%%%%             %%%%               %%%%                                
-                            %%%%            %%%%%%%         %%%%%%                                  
-                            %%%%               %%%%%%%%%%%%%%%%%                                    
-                             %%%%%                %%%%%%%%%                                         
-                             %%%%%%                    %                                            
-                              %%%%%                                                                 
-                               %%%%%                                                                
-                                %%%%%                                                               
-                                 %%%%%                                                              
-                                   %%%%%                                                            
-                                    %%%%%%                                                          
-                                      %%%%%%                                                        
-                                         %%%%%%                                                     
-                                             %%%%%%%
+                      &&                          
+                &&#BGGGGGGBBBGGB#&&               
+             &BGPPPPPPPPPPPPPPPPPPPGGGB&&         
+          &BGPPPPPPPGBB########BBGPPPPPPPG#       
+        #GPPPPPPGB#&              &#BPPPPPPG#     
+      &GPPPPPGB&                      #GPPPPPG&   
+     &GPPPG#&                           BPPPPPP#  
+    #GPPG#                               #PPPB#B  
+   BPPP#                &#BBB###&         BPPP#   
+  #PPPB               #GB#&     &&        &PPPB   
+ &PPPB              &GB                    GPPG   
+ #PPP&             &G#                     GPPP&  
+ &PPG              BG               &      GPPB   
+ #PPB              BP&                     GPG    
+ #PPB              #PB            &&     &GPG     
+ &PPB               BPB       &&        #PP#      
+ &PPP&             &&#GG#&           &#GPB&       
+  BPPB                &#BGGB#&&&##BBGGB#          
+  &PPP#                 &#BBBBBBBB##&             
+   #PPPPB                                         
+    BPPPG&                                        
+     #PPPG                                        
+      &GPPB                                       
+        BPPG#                                     
+         &BPPG&                                   
+           &BGPG#&                                
+              &BGGB#&                             
+                 &#BGB#&&                         
+                      &&&&&                                         
+"
+asciiUbuntu="
+
+                                                  
+                &#BGP55YYYY55PGB#&                
+            &BPYJ????????????????JYPB&            
+          #5J????????????????????????J5#          
+        BY????????????????????!:::~?????YB        
+      #Y?????????????77!!!!7?!     ^??????Y#      
+     G???????????7?!.       :7^...:7????????G     
+    G??????????!: ^7^ ..::..  ^~~~!??????????G    
+   B?????????7:    ~?77?????7~:    :7?????????B   
+   J????????7.   :7????????????!:   .7????????J   
+  B????7!~~7!   :????????????????:   :?????????B  
+  G???7.    ~!  !????????????????7^^^^?????????G  
+  G???7.    ~!  !????????????????7^^^^?????????G  
+  B????7!~~7!   :????????????????:   :?????????B  
+   J????????7.   :!????????????!:   .7????????J   
+   B?????????7:    ~?77?????7~:    :7?????????B   
+    G??????????!: ^7^ ..::..  ^~~~!??????????G    
+     G???????????7?!.       :7^...:7????????G     
+      #Y?????????????77!!!!7?!     ^??????Y#      
+        BY????????????????????!:::~?????YB        
+          #5J????????????????????????J5#          
+            &BPYJ????????????????JYPB&            
+                &#BGP55YYYY55PGB#&                
+                                                                                
+"
+asciiKali="
+               &#GP55YYYYYYYY55PG#&               
+           &BP5YYYYYYYYYYYYYYYYYYYY5PB&           
+        &B5YYYYYYYYYYYYYYYYYYYYYYYYYYYY5B&        
+      &PYJYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYG&      
+     GJJYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYG     
+   P?!!!!!~~~~~~~!77?JYYYYYYYYYYYYYYYYYYYYYYY5&   
+  #YYYYYYYYJJJ??7!~:..:^JYYYYYYYYYYYYYYYYYYYYYY#  
+ G7!!!!~~~~~!!!!!~^:    75YYYJYYYYYYYYYYYYYYYYYY& 
+#???JJJYYYYJ?7!~~~~!!77!:!???7!!7?YYYYYYYYYYYYYY5 
+GJJJJJJ7!~^~!7JJYYYYY5Y7  .:::^^~~~!JYYYYYYYYYYYYB
+YJJJ7~~!?JYYYYYYYYYYYJ: 7YYYYYJ?!~^..7YYYYYYYYYYY5
+JJJJJJYYJJJJJJJJJJJYY^ 75YYYYYYY55Y!:.!JYYYYYYYYYY
+JJJJJJJJJJJJJJJJJJJJY~ !YYYYYYYYYYY55J!JYYYYYYYYY5
+YJJJJJJJJJJJJJJJJJJJJY~ ^7?JJJJJYYYYY55YYYYYYYYYYP
+BJJJJJJJJJJJJJJJJJJJJJYJ!^::::::::^~7?JYYYYYYYYYYB
+ 5JJJJJJJJJJJJJJJJJJJJJYYYYYYYYJJ7~. :^^7YYYYYYYP 
+ &YJJJJJJJJJJJJJJJJJJJJJJJJJJYYYYYYY?^~?!:?YYYY5& 
+  #YJJJJJJJJJJJJJJJJJJJJJJJJJJJYYYYYYY!:JJ:JYY5&  
+   &5JJJJJJJJJJJJJJJJJJJJJJJJJJJJJYYYYY!.YYJYG&   
+     BYJJJJJJJJJJJJJJJJJJJJJJJJJJJJJYYYY^!Y5B     
+      &G5JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJYJJB       
+        &BPYJJJJJJJJJJJJJJJJJJJJJJJJJY5G#         
+           &#G5YJJJJJJJJJJJJJJJJJY5PG#&           
+               &#BGP55YYYYY55PPGB#&                      
 "
 
 # Affichage des infos
@@ -44,4 +98,8 @@ echo "Distribution : $distrib"
 echo "Utilisateur : $user"
 if [ "$distrib" = "Debian GNU/Linux 12 (bookworm)" ]; then
     echo "$asciiDebian"
+elif [ "$distrib" = "Ubuntu 22.04 LTS" ]; then
+    echo "$asciiUbuntu"
+else
+    echo "$asciiKali"
 fi
